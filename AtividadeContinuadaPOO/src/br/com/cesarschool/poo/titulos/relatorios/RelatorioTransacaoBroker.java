49d49eb0cefa46082a3d271607a014ee41da9fbc
velame <mvp2@cesar.school>
2024-11-25 18:51:44 -0300

    quase terminado

---
+++ AtividadeContinuadaPOO/src/br/com/cesarschool/poo/titulos/relatorios/RelatorioTransacaoBroker.java
@@ -0,0 +1,7 @@
+package br.com.cesarschool.poo.titulos.relatorios;
+
+public class RelatorioTransacaoBroker {
+
+	
+	
+}
---
+++ AtividadeContinuadaPOO/src/br/com/cesarschool/poo/titulos/utils/Comparavel.java
@@ -0,0 +1,7 @@
+package br.com.cesarschool.poo.titulos.utils;
+
+public interface Comparavel {
+
+	public abstract int comparar(Comparavel c);
+		
+}
