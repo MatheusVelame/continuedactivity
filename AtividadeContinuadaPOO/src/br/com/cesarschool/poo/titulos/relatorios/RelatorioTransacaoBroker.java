47c84a5b3e04b25c70fc83b4feb58962956c35a6
velame <mvp2@cesar.school>
2024-11-26 02:44:58 -0300

    CONSEGUUIIII

--- AtividadeContinuadaPOO/src/br/com/cesarschool/poo/titulos/relatorios/RelatorioTransacaoBroker.java
+++ AtividadeContinuadaPOO/src/br/com/cesarschool/poo/titulos/relatorios/RelatorioTransacaoBroker.java
@@ -1,7 +1,53 @@
 package br.com.cesarschool.poo.titulos.relatorios;
 
+import java.util.ArrayList;
+
+import br.com.cesarschool.poo.daogenerico.DAOSerializadorObjetos;
+import br.com.cesarschool.poo.daogenerico.Entidade;
+import br.com.cesarschool.poo.titulos.entidades.Transacao;
+import br.com.cesarschool.poo.titulos.utils.ComparadorTransacaoPorNomeCredora;
+import br.com.cesarschool.poo.titulos.utils.Ordenador;
+
 public class RelatorioTransacaoBroker {
 
+	public Transacao[] relatorioTransacaoPorNomeEntidadeCredora() {
+		ComparadorTransacaoPorNomeCredora comparadorTransacaoPorNomeCredora = new ComparadorTransacaoPorNomeCredora();
+		DAOSerializadorObjetos dao = new DAOSerializadorObjetos(Transacao.class);
+        Entidade[] entidades = dao.buscarTodos();
+        
+        ArrayList<Transacao> arrayTransacao = new ArrayList<>(); 
+
+        for (Entidade entidade : entidades) {
+            if (entidade instanceof Transacao) {
+                arrayTransacao.add((Transacao) entidade);
+            }
+        }
+
+        Transacao [] transacoesOrdenadas = arrayTransacao.toArray(new Transacao [0]);
+        
+        Ordenador.ordenar(transacoesOrdenadas, comparadorTransacaoPorNomeCredora);
+        
+        return transacoesOrdenadas;
+        
+	}
 	
+	public Transacao[] relatorioTransacaoPorDataHora() {
+		DAOSerializadorObjetos dao = new DAOSerializadorObjetos(Transacao.class);
+        Entidade[] entidades = dao.buscarTodos();
+        
+        ArrayList<Transacao> arrayTransacao = new ArrayList<>(); 
+
+        for (Entidade entidade : entidades) {
+            if (entidade instanceof Transacao) {
+                arrayTransacao.add((Transacao) entidade);
+            }
+        }
+
+        Transacao [] transacoesOrdenadas = arrayTransacao.toArray(new Transacao [0]);
+        
+        Ordenador.ordenar(transacoesOrdenadas);
+        
+        return transacoesOrdenadas;
+	}
 	
 }
